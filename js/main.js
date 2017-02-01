0e3ea7cc0b5497c4a0b0a67879534f7df22d4a91
NatNantabutr <pnantabu@masonlive.gmu.edu>
2017-02-01 02:16:03 -0500

    Added new js files and update main.js / major changes

--- js/main.js
+++ js/main.js
@@ -8,13 +8,10 @@ var main = function() {
 
   //for collapse nav
   collapseNav();
-
   //INCLUDE .HTML from another file
   $.get("pages/summary.html", function(data) {
     $(".summary").html(data);
     });
-
-
 
     //to set the long scrolling effect
     $('a[href*="#"]:not([href="#"])').on('click', function() {
@@ -24,18 +21,37 @@ var main = function() {
         }, 1500, function(){ });
     });
 
-  
-    $('#aboutME_cell').click(function(){
-      $(this).effect( "slide", 1000 ,getScroll());
+    setDialog('#pro_ex_dialog');
+    setDialog('#edu_dialog');
+    setDialog('#pro_dev_dialog');
+    setDialog('#other_info_dialog');
+    setDialog('#projects_dialog');
+    setDialog('#skills_dialog');
+
+    //to set the effect when clicking on the icon
+    $('#pro_ex').click(function(){
+      $(this).effect( "slide", 1000 ,openDialog("#pro_ex_dialog"));
+    });
+    $('#edu').click(function(){
+      $(this).slideUp(1000).slideDown(1000,openDialog("#edu_dialog"));
+    });
+    $('#pro_dev').click(function(){
+      $(this).fadeOut(1000).fadeIn(1000,setDialog('#pro_dev_dialog'););
+    });
+    $('#other_info').click(function(){
+      $(this).effect("slide", { direction: "right" }, 1000, setDialog('#other_info_dialog'));
+    });
+    $('#projects').click(function(){
+      $(this).hide( "fold", {horizFirst: true }, 1000 );
+      $(this).show( "fold", {horizFirst: true }, 1000 ,setDialog('#projects_dialog'));
+    });
+    $('#skills').click(function(){
+      $(this).hide( "clip", {horizFirst: true }, 1000 );
+      $(this).show( "clip", {horizFirst: true }, 1000,setDialog('#skills_dialog') );
     });
 
-    $('#project_cell').click(function(){
-      $(this).slideUp(1000).slideDown(1000);
-    });
+    
 
-    $('#skills_cell').click(function(){
-      $(this).fadeOut(1000).fadeIn(1000);
-    });
     return false;
 };  
 
@@ -44,14 +60,10 @@ function setDynamicTopPadding(){
   var $topNav_Container = $('div#topNav_Container');
   var topNav_Container_h = $topNav_Container.height();
 
-    
-  //$("body").css("padding-top", topNav_Container_h+'px');
   $("#topNav").css("height", topNav_Container_h+'px');
   $('.page2').css("height", topNav_Container_h+'px');
   $('.page3').css("height", topNav_Container_h+'px');
   $("#page1_inner").css("margin-top", topNav_Container_h+'px');
-  //$("#page2_inner").css("margin-top", topNav_Container_h+'px');
-  //$("#page3_inner").css("margin-top", topNav_Container_h+'px');
 
   var windowHeight = $(window).height();
   //var windowWidth = $(window).width();
@@ -59,32 +71,25 @@ function setDynamicTopPadding(){
   //for page 1
   var page1_inner_h = windowHeight - topNav_Container_h;
   $('#page1_inner').css("height", page1_inner_h+'px');
-  $('#cover_pic:after').css("height", page1_inner_h*0.65+'px');
-
-
-
-
-  var pageOneRowTwo_h = windowHeight/3 - topNav_Container_h;
-  var pageOneRowOne_h = windowHeight - pageOneRowTwo_h - topNav_Container_h;
-  $('.page1_row1').css("height", pageOneRowOne_h+'px');
-  $('.page1_row2').css("height", pageOneRowTwo_h +'px');
-
+  
   //for page 2
   $('#page2_inner').css("height", windowHeight - topNav_Container_h+'px');
+
+  //for page _
   $('#page3_inner').css("height", windowHeight - topNav_Container_h+'px');
 }
 
+//for callback scrolling  after effect 
 function getScroll(param1, callback){
-
     $(param1).click(function(){
       $(this).effect( "slide", 1000);
     });
-
      if (callback && typeof(callback) === "function") {
         callback();
     }
 };
 
+//to create a collapse navigation effect
 function collapseNav(){
   var navbarButton_li = $('#navbarButton_li');
   navbarButton_li.css('list-style', 'none');
@@ -94,7 +99,51 @@ function collapseNav(){
   navbarButton_li.mouseleave(function(){
       $(this).find("ul").css('display', 'none');
     });
+};
+
+function setDialog(dialogID){
+  $(dialogID).dialog({
+               autoOpen: false, 
+               hide: "puff",
+               show : "slide",
+               width: 'auto'             
+  });
+};
+
+function openDialog(dialogID_param){ 
+//if($(dialogID).dialog("isOpen") === false){
+//  alert('open'); 
+//}
+//else{
+  var dialogID = dialogID_param ;
+  var pageToLoad = '';
+  switch (dialogID) { 
+  case '#pro_ex_dialog': 
+    pageToLoad = 'pages/professional_experience.html';
+    break;
+  case '#edu_dialog': 
+     pageToLoad = 'pages/education.html';
+    break;
+  case '#pro_dev_dialog': 
+     pageToLoad = 'pages/pro_development.html';
+    break;    
+  case '#other_info_dialog': 
+      pageToLoad = 'pages/other_info.html';
+    break;
+    case '#projects_dialog': 
+      pageToLoad = 'pages/projects.html';
+    break;
+  default:
+      pageToLoad = 'pages/skills.html';
 }
+
+alert('dialogID :'+dialogID); 
+  $(dialogID).load(pageToLoad, function() {
+        $(dialogID).dialog("open");
+  });
+//}
+};
+
 
 
 $(document).ready(main);
